3e16dea9409af7f636ad309099c666ce8f826f3a
svetaukiyo <stiopok888@gmail.com>
2020-12-05 22:27:58 +0300

    Initial commit

---
+++ FoodsApp-starting-code/app/src/main/java/com/haerul/foodsapp/view/category/CategoryView.java
@@ -0,0 +1,12 @@
+package com.haerul.foodsapp.view.category;
+
+import com.haerul.foodsapp.model.Meals;
+
+import java.util.List;
+
+public interface CategoryView {
+    void showLoading();
+    void hideLoading();
+    void setMeals(List<Meals.Meal> meals);
+    void onErrorLoading(String message);
+}
---
+++ FoodsApp-starting-code/app/src/main/java/com/haerul/foodsapp/view/detail/DetailView.java
@@ -0,0 +1,7 @@
+package com.haerul.foodsapp.view.detail;
+
+import com.haerul.foodsapp.model.Meals;
+
+public interface DetailView {
+    //TODO #4 Add void method  for showLoading, hideLoading, setMeal, onErrorLoading;
+}
